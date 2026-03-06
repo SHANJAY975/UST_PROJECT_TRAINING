26e173dd20b8317b0578fa7db95dd7eac428806a
Shanjay Govindaraju(UST <307387@ust.com>
2026-03-06 14:29:00 +0530

    test

--- Course_2/Project_4/todos/src/main/java/com/ust/springboot/todos/service/UserServiceImpl.java
+++ Course_2/Project_4/todos/src/main/java/com/ust/springboot/todos/service/UserServiceImpl.java
@@ -70,6 +70,8 @@ public class UserServiceImpl implements UserService{
         userRepository.save(user);
     }
 
+
+
     private boolean isOldPasswordCorrect(String currentPassword, String oldPassword){
         return passwordEncoder.matches(oldPassword, currentPassword);
     }
