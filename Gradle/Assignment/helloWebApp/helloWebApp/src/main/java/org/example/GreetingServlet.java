94b8252564371270fd4d498ee374134f4c712dae
SHANJAY975 <shanjay.ssj@gmail.com>
2026-03-11 14:02:01 +0530

    Hello Web App Assignment completed

---
+++ Gradle/Assignment/helloWebApp/helloWebApp/src/main/java/org/example/GreetingServlet.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.io.IOException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/username")
+public class GreetingServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        request.setAttribute("username","Hello "+username);
+        request.getRequestDispatcher("response.jsp").forward(request, response);
+    }
+}
---
+++ Gradle/Assignment/helloWebApp/helloWebApp/src/main/webapp/response.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Greet Page</title>
+</head>
+<body>
+    <h2>${username}</h2>
+</body>
+</html>
