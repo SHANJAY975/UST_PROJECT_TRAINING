3184c6daced23029e7b404efc3c3f5b20d13a05c
SHANJAY975 <shanjay.ssj@gmail.com>
2026-03-12 14:43:31 +0530

    Custom Plugin created

---
+++ Gradle/firstWebApp/buildSrc/src/main/groovy/org.example/MyAWSPlugin.groovy
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+public class MyAWSPlugin implements Plugin<Project>{
+
+    @Override
+    public void apply(Project project) {
+        project.task("copyToS3"){
+            doLast {
+                println "Copied to S3"
+            }
+        }
+        project.task("deployToEC2"){
+            doLast {
+                println "Application is up and running on AWS EC2"
+            }
+        }
+    }
+}
